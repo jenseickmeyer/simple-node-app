d3c42dde3549fdae45142194e204306b28be621c
Jens Eickmeyer <jens.eickmeyer@carpinuslabs.com>
2019-07-18 10:13:00 +0200

    Print the start of the current day

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 // Use moment.js for date and time operations
 const moment = require('moment');
 
-// Print out the current date and time
-console.log(moment().format());
+// Print out the date and time of the beginning of today
+console.log(moment().startOf('day').format());
